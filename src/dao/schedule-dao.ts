3f757c1bb9d0c4306cd33e91fc7bc1afe35b82d1
Sayuranga Fernando <sarindusauranga@gmail.com>
2025-03-17 21:21:25 +0530

    feat: schedule mangement
    schedule view by id ,update and deete

--- src/dao/schedule-dao.ts
+++ src/dao/schedule-dao.ts
@@ -22,8 +22,71 @@ export default class ScheduleDAO {
             return await Schedule.find();
         } catch{
 
-            throw new Error("Error creating Schedule");
+            throw new Error("Error fetching all Schedules");
 
         }
     }
-}
+
+
+    //Get Schedule by ID
+    static async getScheduleById(id: string){
+        try {
+            return await Schedule.findById(id);
+        } catch{
+
+            throw new Error("Error Fetching Schedule");
+
+       }    
+    }
+
+    //Update Schedule
+    static async updateSchedule(id: string, scheduleData: IScheduleAttributes){
+        try {
+
+            if(!mongoose.Types.ObjectId.isValid(id)){
+                throw new Error("Invalid Schedule ID");
+            }
+
+            const updatedSchedule = await Schedule.findByIdAndUpdate(id, scheduleData, { new: true });
+
+            if(!updatedSchedule){
+                throw new Error("Schedule not found");
+            }
+
+            return updatedSchedule;
+
+
+        }catch{
+
+            throw new Error("Error updating Schedule");
+
+       }
+    } 
+
+    //Delete Schedule
+    static async deleteSchedule(id: string){
+        try {
+
+            if(!mongoose.Types.ObjectId.isValid(id)){
+                throw new Error("Invalid Schedule ID");
+            }
+
+            const deletedSchedule = await Schedule.findByIdAndUpdate(
+                id,
+                { isActive: 0 },
+                { new: true }
+            );
+
+            if(!deletedSchedule){
+                throw new Error("Schedule not found");
+            }
+
+            return deletedSchedule;
+
+        }catch{
+
+            throw new Error("Error Deleting Schedule");
+
+       }
+    }
+} 
--- src/end-points/schedule-ep.ts
+++ src/end-points/schedule-ep.ts
@@ -6,7 +6,7 @@ import { IAllocatedeqAttributes } from "../models/allocatedeq-model";
 
 const router = Router();
 
-
+//create schedule route
 router.post("/create", async (req, res) => {
     try {
         const { shedule_name, shedule_type, date, time_slot, place, description, capacity } = req.body;
@@ -37,6 +37,8 @@ router.post("/create", async (req, res) => {
     }
 });
 
+
+//view all schedules route
 router.get("/viewAll", async (req, res) => {
     try {
         const schedules = await ScheduleDAO.getAllSchedules();
@@ -46,5 +48,85 @@ router.get("/viewAll", async (req, res) => {
     }
 });
 
+//view schedule by id route
+
+router.get("/view/:id", async (req, res) => {
+    try {
+        const scheduleId = req.params.id;
+        const schedule = await ScheduleDAO.getScheduleById(scheduleId);
+
+        if (!schedule) {
+            return res.status(404).json({ message: "Schedule not found" });
+        }
+
+        res.status(200).json({message: "Schedule fetched successfully",schedule});
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error", error });
+    }
+
+});
+
+//update schedule route
+
+router.post("/update", async (req, res) => {
+
+    try {
+        const { scheduleId, shedule_name, shedule_type, date, time_slot, place, description, capacity } = req.body;
+
+        if(!scheduleId){
+            return res.status(400).json({ message: "Schedule ID is required" });
+        }
+        if (!shedule_name || !shedule_type || !date || !time_slot || !place || !description || !capacity) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        const scheduleData: IScheduleAttributes = {
+            shedule_name,
+            shedule_type,
+            date,
+            time_slot,
+            place,
+            capacity,
+            description,
+            status: 1,
+            isActive: 1,
+        };
+
+        const schedule = await ScheduleDAO.updateSchedule(scheduleId, scheduleData);
+
+        if (!schedule) {
+            return res.status(404).json({ message: "Schedule not found" });
+        }
+
+        res.status(200).json({ message: "Schedule updated successfully",schedule });
+
+    }catch (error) {
+        res.status(500).json({ message: "Internal Server Error", error });
+    }
+
+});
+
+//delete schedule route
+router.post("/delete", async (req, res) => {
+    try {
+        const scheduleId = req.body.scheduleId;
+
+        if (!scheduleId) {
+            return res.status(400).json({ message: "Schedule ID is required" });
+        }
+
+        const schedule = await ScheduleDAO.deleteSchedule(scheduleId);
+
+        if (!schedule) {
+            return res.status(404).json({ message: "Schedule not found" });
+        }
+
+        res.status(200).json({ message: "Schedule deleted successfully", schedule });
+
+    }catch (error) {
+        res.status(500).json({ message: "Internal Server Error", error });
+    }
+
+});
 
 export default router;
